a148f65a24b0096958909c578877ef5778359f0d
Mangeshlawande <mangeshlawande223@gmail.com>
2024-06-01 13:25:05 +0530

    Add new file

---
+++ Problems.py
@@ -0,0 +1,15 @@
+# write a program that prompt the user for two number and display the addition, sub, mult, and div between them ;
+num1 = float(input("Enter first num: "));
+num2 = float(input("Enter second number :"));
+
+add = num1 + num2;
+sub = num1 - num2;
+mult = num1 * num2;
+div = num1  / num2;
+
+print("addition", add);
+print("subtraction", sub);
+print("Mult", mult);
+print("division", div);
+
+
--- oop.py
+++ oop.py
@@ -1,6 +1,24 @@
 # name  = input("Enter your name :");
 # print("Hello "+name+"!");
 
-age  =  input("Enter Your age:");
-age = int(age);
-print("You will be "+ str(age +1) + " Yrs.");
+# age  =  input("Enter Your age:");
+# age = int(age);
+# print("You will be "+ str(age +1) + " Yrs.");
+
+# Math operation 
+
+# Addition (+)
+# Div(/)
+# Floor  Div(//): divide the number and round down nearest whole number 
+# Modulo (%) :return  remainder after division 
+# Exponentiation (**): 
+# Raises nuomber to the power of another number.
+# abs():  fun() return the positive number 
+# round() :  round the number to nearest integer 
+# result = round(x, 2) : 3.14
+# Square root(math.sqrt()): calculate square root of number
+
+import math
+x = 4;
+r = math.sqrt(x);
+
