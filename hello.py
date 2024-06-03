938930d10a902260b0fc79e0589755f3a73af4b2
Mangeshlawande <mangeshlawande223@gmail.com>
2024-06-03 14:27:57 +0530

    Add files

---
+++ hello.py
@@ -0,0 +1,21 @@
+import os
+# import tensorflow
+# import flask = it is used for building websites -->>  pip install flask
+
+print("Welcome to My world!!")
+print('hello World!!')
+# .\hello.py-           
+''' THis is also a comment just like above line '''
+# print statement 
+print(''' Twinkle, twinkle ,littlestar ,
+      how i wonder what you are !
+      Up above the world so high ,
+      Like a diamnd in the  sky.''')
+
+# Q2.  use the REPL  and print the table of 5 
+# Q3 . install the external module and use it to perform an operation of your interest.
+# play sound module
+
+# rite a pythn program to print the cntents of directrory using os module .searsh online for the function which deserve that
+
+print(os.listdir()) 
---
+++ python.py
@@ -0,0 +1,37 @@
+name = "Mangesh"
+# print(name)
+
+greeting = '''Biological Diversity --> The variety that we see in the Living things\n\t which is exist on the Earth\\Globe\'s Is called biological-diversity. '''
+
+# print(greeting + name)
+# print(type(name))
+
+# concatinating two string
+c = greeting 
+# print(c)
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+
+#  we cannot modify the string 
+# name[3] = 'd' --> not work
+
+#  slicing 
+# print(name[0:5])
+
+# length function 
+
+# print(name[-4:-1])  # is same as name [1:4]
+
+# print(c[0:5:2])
+# print(len(c))
+# print(c.endswith("mangesh"))
+# print(c.count('M'))
+# print(c.capitalize())
+# print(c.find("sh"))
+print(c.replace("e","E"))
+
